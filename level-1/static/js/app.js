4e3be5f7303d0bea005bbf05fafc87a1941796f1
Rachel Zhang <mailrenxu@hotmail.com>
2021-08-14 11:19:57 +1000

    update to load datatable into webseit

--- level-1/static/js/app.js
+++ level-1/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// use d3 to select the table body
+var tbody = d3.select("tbody");
+console.log(tbody);
+
+// iterate through each array in the table data
+tableData.forEach((ufodata)=>{
+    var row=tbody.append("tr");
+    Object.entries(ufodata).forEach(([key,value])=>{
+        var cell=tbody.append("td");
+        cell.text(value);
+    });
+});
